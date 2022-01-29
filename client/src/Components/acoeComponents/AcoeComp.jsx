008988f274ba3c214d534f58524d18b1ceb590e1
NarenCs-19 <narendirakumar2002@gmail.com>
2022-01-29 12:41:48 +0530

    sideBar commit

--- client/src/Components/acoeComponents/AcoeComp.jsx
+++ client/src/Components/acoeComponents/AcoeComp.jsx
@@ -5,14 +5,20 @@ import AcoePendingApprovals from './AcoePendingApprovals';
 
 function AcoeComp() {
     const [compID,setcompID] = useState(1);
+    const [showMenu,setshowMenu] = useState(1);
+
+    const handleShowMenu = ()=>{
+        console.log(showMenu);
+        setshowMenu(showMenu+1)
+    }
     return (
         <div className="container-fluid">
             <div className="row">
-                <div className="side-nav col-1 m-0 p-0">
-                <AcoeSectionComp/>
+                <div className="side-nav m-0 p-0"  id={showMenu%2==0?"close":null}>
+                <AcoeSectionComp menuHandler={handleShowMenu}/>
                 </div>
-                <div className="mainComp col">
-                    {compID === 1 && <AcoePendingApprovals/>}
+                <div className="mainComp">
+                    {/* {compID === 1 && <AcoePendingApprovals/>} */}
                 </div>
             </div>
         </div>
--- client/src/Components/acoeComponents/AcoeSectionComp.jsx
+++ client/src/Components/acoeComponents/AcoeSectionComp.jsx
@@ -3,57 +3,80 @@ import DashboardIcon from "@mui/icons-material/Dashboard";
 import PendingActionsIcon from "@mui/icons-material/PendingActions";
 import QuestionMarkIcon from "@mui/icons-material/QuestionMark";
 import PowerSettingsNewIcon from "@mui/icons-material/PowerSettingsNew";
+import { useState } from "react";
+import avatar from "./assets/avatar.svg";
 
 function AcoeSectionComp(props) {
+  
+  
   return (
-    <div className="sidenav row d-flex m-0 p-0">
-      <div className="navItems col-12 p-0">
-        <div className="nav-items-icons row col-12">
-          <ul className="navbar-nav text-uppercase fw-bold row">
-            <li className="nav-item col-12 d-flex align-items-center py-4">
-              <DashboardIcon />
-            </li>
-            <li className="nav-item col-12  d-flex align-items-center py-4">
-              <PendingActionsIcon />
-            </li>
-            <li className="nav-item col-12  d-flex align-items-center py-4">
-              <QuestionMarkIcon />
-            </li>
-            <li className="nav-item col-12  d-flex align-items-center py-4">
-              <PowerSettingsNewIcon />
-            </li>
-          </ul>
+    <div className="sidenav row d-flex flex-column m-0 p-0">
+      <div className="profile-container col-12 mt-3 px-0">
+        <div className="profile-pic text-center">
+        <img src={avatar} alt="avatar" />
         </div>
-        <div className="nav-items-names">
+        <div className="profile-details">
+          <div className="profile-name">
+            Brats
+          </div>
+          <div className="profile-job">Student</div>
+        </div>
+        <div className="menu-icon" onClick={props.menuHandler}>
+          <box-icon name="menu"></box-icon>
+        </div>
+      </div>
+      <div className="navItems col-12 p-0">
           <ul className="text-uppercase fw-bold row">
-            <li className="nav-item col-12 d-flex align-items-center py-4">
+            <li className="nav-item col-12 d-flex align-items-center">
+              <div className="navItem-icon"><DashboardIcon /></div>
+              <div className="nav-link-text">
               <a
-                className="nav-link active text-white"
+                className="nav-link active"
                 aria-current="page"
                 href="/acoe/Dashboard"
               >
                 Dashboard 
               </a>
+              </div>
             </li>
-            <li className="nav-item col-12  d-flex align-items-center py-4">
-              <a className="nav-link text-white" href="/acoe/PendingApprovals">
-                Pending approvals
+            <li className="nav-item col-12  d-flex align-items-center">
+            <div className="navItem-icon"><PendingActionsIcon /></div>
+              <div className="nav-link-text">
+              <a
+                className="nav-link active"
+                aria-current="page"
+                href="/acoe/Dashboard"
+              >
+               Approvals 
               </a>
+              </div>
             </li>
-            <li className="nav-item col-12  d-flex align-items-center py-4">
-              <a className="nav-link text-white" href="/acoe/FAQ">
-                FAQ
+            <li className="nav-item col-12  d-flex align-items-center">
+            <div className="navItem-icon"><QuestionMarkIcon/></div>
+              <div className="nav-link-text">
+              <a
+                className="nav-link active"
+                aria-current="page"
+                href="/acoe/Dashboard"
+              >
+                FAQs
               </a>
+              </div>
             </li>
-            <li className="nav-item col-12  d-flex align-items-center py-4">
-              <a className="nav-link text-white" href="/acoe/Logout">
+            <li className="nav-item col-12  d-flex align-items-center">
+            <div className="navItem-icon"><PowerSettingsNewIcon /></div>
+              <div className="nav-link-text">
+              <a
+                className="nav-link active"
+                aria-current="page"
+                href="/acoe/Dashboard"
+              >
                 Logout
               </a>
+              </div>
             </li>
           </ul>
-        </div>
-      </div>
-      
+      </div>      
     </div>
   );
 }
